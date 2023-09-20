00a14018eaa56be255c9b0d622db81cca8b167f0
Ivan <itverdyy@gmail.com>
2023-09-21 00:14:34 +0300

    made next and prev private, nulled node

--- main.cpp
+++ main.cpp
@@ -3,65 +3,81 @@
 
 using namespace std;
 
-class node
-{
-public:
-    node *next;
-    node *prev;
-    void SetValue(int setV)
-    {
-        value = setV;
-    }
-    int GetValue()
-    {
-        return value;
-    }
+class node {
 private:
     int value;
+    node *next;
+    node *prev;
+public:
+    node() {
+        next = prev = nullptr;
+        value = 0;
+    }
+
+    void setValue(int setV) {
+        value = setV;
+    }
+
+    int getValue() {
+        return value;
+    }
+
+    node *getNext() {
+        return next;
+    }
+
+    node *getPrev() {
+        return prev;
+    }
+
+    void setNext(node *nextNode) {
+        next = nextNode;
+    }
+
+    void setPrev(node *prevNode) {
+        prev = prevNode;
+    }
 };
 
-class LIST{
+class LIST {
 private:
     node *head, *tail;
 public:
-    LIST()
-    {
+    LIST() {
         head = tail = nullptr;
     }
-    void AddLastNode(int data)
-    {
-        node* temp = new node;
-        temp->SetValue(data);
-        if(!head)
-        {
-            temp->next=temp->prev=temp;
-            head=tail=temp;
-        }
-        else
-        {
-            head->prev=temp; temp->next=head;
-            tail->next=temp; temp->prev=tail;
-            tail=temp;
+
+    void AddLastNode(int data) {
+        node *temp = new node;
+        temp->setValue(data);
+        if (!head) {
+            temp->setNext(temp);
+            temp->setPrev(temp);
+            head = tail = temp;
+        } else {
+            head->setPrev(temp);
+            temp->setNext(head);
+            tail->setNext(temp);
+            temp->setPrev(tail);
+            tail = temp;
         }
     }
-    void ShowNode()
-    {
-        node *temp=head;
-        int i=1;
-        do
-        {
-            cout <<"Data in node "<< i << " = "<< temp->GetValue() << "\n";
-            temp=temp->next;
+
+    void ShowNode() {
+        node *temp = head;
+        int i = 1;
+        do {
+            cout << "Data in node " << i << " = " << temp->getValue() << "\n";
+            temp = temp->getNext();
             i++;
-        }
-        while(temp!=head);
+        } while (temp != head);
     }
 };
 
 int main() {
     LIST newList;
-    for(int i=0; i<8; i++) {
-        newList.AddLastNode(i*5);
+    for (int i = 0; i < 8; i++) {
+        newList.AddLastNode(i * 5);
     }
     newList.ShowNode();
     return 0;
